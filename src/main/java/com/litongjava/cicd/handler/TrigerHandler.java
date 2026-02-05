832902f991940ba707f5513b8d5acb6235a1f34d
litongjava <litongjava001@gmail.com>
2026-02-05 14:52:09 +0800

    update TrigerHandler

--- src/main/java/com/litongjava/cicd/handler/TrigerHandler.java
+++ src/main/java/com/litongjava/cicd/handler/TrigerHandler.java
@@ -6,13 +6,14 @@ import com.litongjava.tio.boot.http.TioRequestContext;
 import com.litongjava.tio.http.common.HttpRequest;
 import com.litongjava.tio.http.common.HttpResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TrigerHandler {
   public HttpResponse trigger(HttpRequest request) {
+    log.info("body:{}", request.getBodyString());
     String projectName = request.getString("name");
-    String protocol = request.getRequestLine().getProtocol();
-    String host = request.getHost();
-    String requestURI = request.getRequestURI();
-    String requestURL = protocol.toLowerCase() + "://" + host + requestURI;
+    String requestURL = "Hide";
     RespBodyVo respVo = TrigerService.triger(projectName, requestURL);
     return TioRequestContext.getResponse().setJson(respVo);
   }
